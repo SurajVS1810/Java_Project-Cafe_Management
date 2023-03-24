41fc1e3f6a5cca33a3da5db4bbc5c3396f15846b
Suraj <surajsubrahmanyn@gmail.com>
2023-03-25 01:19:07 +0530

    food changes

--- Food.java
+++ Food.java
@@ -31,11 +31,12 @@ public class Food extends JFrame {
 	 */
 	public Food() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500,500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
